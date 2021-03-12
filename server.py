39f16ce519df6d79de33a20391b39ca0bb4a5fbd
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-03-12 11:34:00 -0700

    changed register listener process

--- server.py
+++ server.py
@@ -125,7 +125,7 @@ def clear():
 @app.route("/listener/<id>", methods=['POST','PUT'])
 def add_listener(id):
     myWorld.add_listener( id )
-    return flask.jsonify(myWorld.world())
+    return flask.jsonify(dict())
 
 @app.route("/listener/<id>")    
 def get_listener(id):
